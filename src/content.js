25e4fcc4d80dede2895bc64a5faf4e6d8ad533e7
Aurélien Hervé <mail@aurelien-herve.com>
2017-11-05 23:48:23 +0100

    fix(add timeout to boom button seems to improve robustness):

--- src/content.js
+++ src/content.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener((msg, sender, cb) => {
   if (msg.action === 'play' && !!msg.url) {
-    play (msg.url)
+    setTimeout(() => {
+      play (msg.url)
+    })
     cb(null)
   }
 })
