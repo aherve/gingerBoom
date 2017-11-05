607c4c6292d09a96a2d85232d1b96d434c29b21d
Aurélien Hervé <mail@aurelien-herve.com>
2017-11-05 20:10:32 +0100

    cleanup even when video crashes

--- src/play.js
+++ src/play.js
@@ -4,6 +4,7 @@ function play (videoSrc) {
 
   const video = document.createElement("video");
   video.classList.add('boom-button')
+  video.classList.add('clearfix')
   const autoplay = document.createAttribute('autoplay')
   const style = document.createAttribute('style')
   style.value = 'position: fixed; right: 0; top: 0; z-index: 1000;'
@@ -25,17 +26,18 @@ function play (videoSrc) {
   video.appendChild(source);
 
   video.addEventListener('ended', () => fadeOut(video))
+  // close after 4s whatever happens
+  setTimeout(() => fadeOut(video), 4000)
 
   const body = document.body;
   body.prepend(video)
 }
 
 function fadeOut(videoNode) {
+  if (!videoNode) {return}
   videoNode.style.opacity = 0;
   setTimeout(() => {
     videoNode.pause();
-    videoNode.src = "";
-    videoNode.load();
-    videoNode.parentNode.removeChild(videoNode)
+    videoNode.remove()
   }, 300)
 }
